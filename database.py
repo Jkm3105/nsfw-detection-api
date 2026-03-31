180b60a87ced34836f6c28cce5d375b269f93cb8
SRT <jashmehta92@gmail.com>
2026-03-31 14:58:19 +0530

    Fix SSL connection for MongoDB Atlas

--- database.py
+++ database.py
@@ -4,8 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(os.getenv("DB_URL"))
-db = client[os.getenv("DB_NAME")]
+def get_db():
+    client = MongoClient(
+        os.getenv("MONGO_URI"),
+        tls=True,
+        tlsAllowInvalidCertificates=True
+    )
+    db = client[os.getenv("DB_NAME")]
+    return db
+
+db = get_db()
 logs_collection = db["detection_logs"]
 
 def save_log(log_entry: dict):
